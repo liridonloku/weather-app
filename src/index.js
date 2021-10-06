0324a7de57fd2ad976c798da1c891bdc491c1551
Liridon Loku <liridonloku@gmail.com>
2021-10-06 15:46:20 +0200

    Add search form and style display

--- src/index.js
+++ src/index.js
@@ -1,25 +1,56 @@
 import './style.css';
+
+const cityName = document.querySelector('.city-name');
+const weatherIcon = document.querySelector('.weather-icon');
+const temperature = document.querySelector('.temp-value');
+const units = document.querySelector('.units');
+const form = document.getElementById('city');
+const search = document.getElementById('search');
+let defaultUnits = 'metric';
+
+search.addEventListener('click', () => {
+    const city = form.value;
+    getWeather(city);
+})
+
+units.addEventListener('click', () => {
+    if(units.textContent != '-'){
+        if(defaultUnits === 'metric'){
+            defaultUnits = 'imperial';
+        } else {
+            defaultUnits = 'metric';
+        }
+        getWeather(cityName.textContent);
+    }
+})
+
+
 const content = document.querySelector('.content');
-async function getWeather() {
+async function getWeather(city) {
     loadingScreen();
-    const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=london&appid=a04537d2b453a6d8a3d650771653a893&units=metric');
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=a04537d2b453a6d8a3d650771653a893&units=${defaultUnits}`);
     const data = await response.json();
     displayWeather(data);
+    console.log(data);
 }
 
 const displayWeather = (data) => {
-    content.replaceChildren();
-    const cityName = document.createElement('h1');
+    const icon = document.createElement('img');
+    icon.setAttribute('src', `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`);
+    weatherIcon.textContent = '';
+    weatherIcon.appendChild(icon);
     cityName.textContent = data.name;
-    const temperature = document.createElement('p');
     temperature.textContent = data.main.temp;
-    content.appendChild(cityName);
-    content.appendChild(temperature);
+    if(defaultUnits === 'metric'){
+        units.textContent = '°C';
+    }else{
+        units.textContent = '°F';
+    }
 }
 
 const loadingScreen = () => {
-    content.replaceChildren();
-    content.textContent = 'Loading';
+    cityName.textContent = 'Loading...';
+    weatherIcon.textContent = '-';
+    temperature.textContent = '-';
+    units.textContent = '-';
 }
-
-getWeather();
